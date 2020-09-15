d353ce2d38ce2f55f0aa4cbb942ddb0a1d1ca44f
chrispinkney <hey@chrispinkney.com>
2020-09-15 11:14:57 -0400

    finished file-arg

--- Releases/0.1/webscraper.py
+++ Releases/0.1/webscraper.py
@@ -10,15 +10,28 @@ if len(sys.argv) == 1:
 
 # Parse the command line arguments
 parser = argparse.ArgumentParser(description='See below for optional flags.')
-parser.add_argument('-u', '--url', metavar='', required=True, help='The url to check for broken links (must start with http://).')
-parser.add_argument('-f', '--file', metavar='', help='Checks through a specified html file (not yet implemented.)')
+parser.add_argument('-u', '--url', '-url', metavar='', help='The url to check for broken links. Example: -u http://google.ca')
+parser.add_argument('-f', '--file', '-file', metavar='', help='Checks through a specified html file that is located in the current working directory. Example: -f index.html')
 args = parser.parse_args()
 
-print(args)
+def link_check(soup):
+    for link in soup.find_all('a'):
+        l = link.get('href')
+        req = requests.get(l)
+        if (req.status_code == 200):
+            print(f"SUCCESSFUL LINK: {l}")
+        elif (req.status_code == 300):
+            print(f"REDIRECTED LINK: {l}")
+        elif (req.status_code == 400 or req.status_code == 404):
+            print(f"DEAD LINK (400 ERROR): {l}")
+        elif (req.status_code == 500):
+            print(f"DEAD LINK (500 ERROR): {l}")
+        else: 
+            print(f"UNKNOWN LINK: {l}")
 
 def url_check(url):
     # Create a request from the command line arg
-    req = requests.get(args.url)
+    req = requests.get(args.url) #i dont think this is actually using the url object lul, fix this
 
     # Send the req to BS4 for parsing
     soup = BeautifulSoup(req.text, "html.parser")
@@ -36,4 +49,12 @@ def url_check(url):
         else: 
             print(f"UNKNOWN LINK: {a['href']}")
 
-url_check(args)
+def file_check():
+    with open(args.file, 'r') as f:
+        soup = BeautifulSoup(f, "html.parser")
+        link_check(soup)
+
+if (args.url):
+    url_check(args)
+if (args.file):
+    file_check()
