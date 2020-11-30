13cf0732778a90355b9a5b73e52db058eae2c06c
Chris Pinkney <hey@chrispinkney.com>
2020-11-29 22:52:58 -0500

    added license to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 import setuptools
 
-with open("docs/README.md", "r") as fh:
-    long_description = fh.read()
+with open("docs/README.md", "r") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="He's Dead, Jim",
-    version="1.0.4",
+    version="1.0.5",
     author="Chris Pinkney",
     author_email="hey@chrispinkney.com",
     description="A command-line tool for finding and reporting dead/broken links in a file or webpage.",
+    url="https://github.com/chrispinkney/He-s-Dead-Jim",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/chrispinkney/He-s-Dead-Jim",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         "argparse == 1.4.0",
         "requests == 2.24.0",
@@ -25,11 +31,6 @@ setuptools.setup(
         "pytest-cov == 2.10.1",
     ],
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     entry_points={
         "console_scripts": [
             "hdj = src.hdj:main_wrapper",
